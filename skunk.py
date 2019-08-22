35e3a4195b882524910ba02d07481b08267cdc6f
Anders Berkeman <anders.berkeman@ericsson.com>
2019-08-22 14:48:13 +0200

    [skunk] check is now a function

--- skunk.py
+++ skunk.py
@@ -21,30 +21,38 @@ class nndata():
 
 
 
+def checkconvlayer(N):
+	print(N.w_in, N.h_in, N.c_in, '->', N.w_ut, N.h_ut, N.c_ut)
+	print(N.k, N.groups, N.stride, N.pad)
+	print()
+
+	# @@@ Still only one output channel
+	# @@@ Does not handle groups
+
+	for h in range(0, N.h_in, N.stride):
+		for w in range(0, N.w_in, N.stride):
+			t = 0.0
+			for cc in range(0, N.c_in): # channel
+				for x in range(-(N.k//2), N.k//2 + 1):
+					for y in range(-(N.k//2), N.k//2 + 1):
+						ww = w + x
+						hh = h + y
+						if ww < 0 or ww > N.w_in: continue
+						if hh < 0 or hh > N.h_in: continue
+						t += N.inputs[ww + hh*N.w_in + cc*N.w_in*N.h_in] * N.weights[x + N.pad + N.k*(y + N.pad) +cc*N.k*N.k]
+#						print(w, h, x, y, cc, ww + hh*N.w_in + cc*N.w_in*N.h_in, x + N.pad + N.k*(y + N.pad) +cc*N.k*N.k)
+
+			y = N.outputs[w//N.stride + (h//N.stride)*N.w_ut]
+			print(w, h, t, y, t-y, abs(t-y) < 1e-5)
+
+
+
 N = nndata('darknet_run.txt')
 
 N.nextlayer()
+#checkconvlayer(N)
 N.nextlayer()
+#checkconvlayer(N)
 
-print(N.w_in, N.h_in, N.c_in, '->', N.w_ut, N.h_ut, N.c_ut)
-print(N.k, N.stride, N.pad)
-print()
-
-# @@@ Still only one output channel
-# @@@ Does not handle groups
-
-for h in range(0, N.h_in, N.stride):
-	for w in range(0, N.w_in, N.stride):
-		t = 0.0
-		for cc in range(0, N.c_in): # channel
-			for x in range(-(N.k//2), N.k//2 + 1):
-				for y in range(-(N.k//2), N.k//2 + 1):
-					ww = w + x
-					hh = h + y
-					if ww < 0 or ww > N.w_in: continue
-					if hh < 0 or hh > N.h_in: continue
-					t += N.inputs[ww + hh*N.w_in + cc*N.w_in*N.h_in] * N.weights[x + N.pad + N.k*(y + N.pad) +cc*N.k*N.k]
-					print(w, h, x, y, cc, ww + hh*N.w_in + cc*N.w_in*N.h_in, x + N.pad + N.k*(y + N.pad) +cc*N.k*N.k)
-
-		y = N.outputs[w//N.stride + (h//N.stride)*N.w_ut]
-		print(w, h, t, y, t-y, abs(t-y) < 1e-5)
+N.nextlayer()
+checkconvlayer(N)
