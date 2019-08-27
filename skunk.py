d212d9cef681147ee18e426bed0cbbef307bc0f2
Anders Berkeman <anders.berkeman@ericsson.com>
2019-08-27 10:04:35 +0200

    [skunk] rework, 3x3x3 and 1x1x32 works

--- skunk.py
+++ skunk.py
@@ -27,7 +27,8 @@ def checkconvlayer(N):
 
 	W = N.w_in
 	H = N.h_in
-	C = N.c_in
+	CI = N.c_in
+	CU = N.c_ut
 	G = N.groups
 	P = N.pad
 	K = N.k
@@ -39,25 +40,27 @@ def checkconvlayer(N):
 
 	for h in range(0, N.h_in, N.stride):
 		for w in range(0, N.w_in, N.stride):
-			for g in range(0, N.groups):
-				t = 0.0
-				for c in range(0, C//G):
-					for y in range(-(K//2), K//2 + 1):
-						for x in range(-(K//2), K//2 + 1):
-							ww = w + x
-							hh = h + y
-							if ww < 0 or ww >= W: continue
-							if hh < 0 or hh >= H: continue
+			for cu in range(0, CU):
+				for g in range(0, N.groups):
+					t = 0.0
+					for ci in range(0, CI//G):
+						for y in range(-(K//2), K//2 + 1):
+							for x in range(-(K//2), K//2 + 1):
+								ww = w + x
+								hh = h + y
+								if ww < 0 or ww >= W: continue
+								if hh < 0 or hh >= H: continue
 
-							six =  ww     + W*hh      + W*H*(c + g*C//G)
-							wix = (x + P) + K*(y + P) + K*K*(c + g*C//G)
-							tix = w//N.stride + (h//N.stride)*N.w_ut + g*H*W//N.stride//N.stride
-							t += N.inputs[six] * N.weights[wix]
+								six =  ww     + W*hh      + W*H*ci + W*H*CI//G*g
+								wix = (x + P) + K*(y + P) + K*K*ci + K*K*CI//G*cu
+								tix = (w//N.stride) + (h//N.stride)*N.w_ut + (H*W//N.stride//N.stride)*cu
+								t += N.inputs[six] * N.weights[wix]
+#								print('b %6d %6d %6d' % (six, wix, tix,))
 				yy = N.outputs[tix]
-#				print(w, h, '-', c, g, '-', t, yy, t-yy, abs(t-yy) < 1e-5)
 				if abs(t - yy) >= 1e-5:
+					print(w, h, '  ', x, y, '  ', ci, cu, '     ', t, yy)
 					errs += 1
-				maxerr = max(maxerr, abs(t-yy))
+					maxerr = max(maxerr, abs(t-yy))
 	print('errs', errs)
 	print('maxerr', maxerr)
 
