8d93a8285ef909d64983186e28acebead058ae99
Anders Berkeman <anders.berkeman@ericsson.com>
2019-09-09 14:22:50 +0200

    [complexity2] print cache sizes from iterator

--- dev/a_complexity2.py
+++ dev/a_complexity2.py
@@ -1,26 +1,17 @@
-import blob
-
-
 datasets=('reslayers',)
 
 def synthesis():
 
 	print(datasets)
 
-	for item in datasets.reslayers.iterate(None, ()):
-		print(item)
-
-
-	# reslayers, remlayers = blob.load(jobid=jobids.source)
-
-	# for item in reslayers:
-	# 	print()
-	# 	print("%dx%d-%d -> %dx%d-%d -> %dx%d-%d" % (
-	# 		item.wi, item.hi, item.ci,
-	# 		item.wi, item.hi, item.cm,
-	# 		item.wo, item.ho, item.co,
-	# 	))
-	# 	inputfeature  = item.wi * item.hi * item.ci
-	# 	twolinechache = item.wi * 2 * item.ci
-	# 	dwcache9      = 9* item.cm
-	# 	print(inputfeature, twolinechache, dwcache9)
+	for wi, hi, ci, cm, co, wo, ho in datasets.reslayers.iterate(None, ('wi', 'hi', 'ci', 'cm', 'co', 'wo', 'ho',)):
+		print()
+		print("%dx%d-%d -> %dx%d-%d -> %dx%d-%d" % (
+			wi, hi, ci,
+			wi, hi, cm,
+			wo, ho, co,
+		))
+		inputfeature  = wi * hi * ci
+		twolinechache = wi * 2 * ci
+		dwcache9      = 9 * cm
+		print(inputfeature, twolinechache, dwcache9)
