01a0573de0f1cc3f11d48c5ca0d0b5643e8afd41
Anders Berkeman <anders.berkeman@ericsson.com>
2019-09-09 14:42:45 +0200

    [complexity2] add cc est + options.N

--- dev/a_complexity2.py
+++ dev/a_complexity2.py
@@ -1,10 +1,13 @@
+from math import ceil
+
 datasets=('reslayers',)
+
+options = dict(N = 16)
 
 def synthesis():
 
-	print(datasets)
-
 	m = 0
+	c = 0
 
 	for wi, hi, ci, cm, co, wo, ho in datasets.reslayers.iterate(None, ('wi', 'hi', 'ci', 'cm', 'co', 'wo', 'ho',)):
 		print()
@@ -17,7 +20,16 @@ def synthesis():
 		mac1 = wi*hi * ci * cm
 		mac2 = wo*ho *  9 * cm
 		mac3 = wo*ho * cm * co
-		print(mac1, mac2, mac3, mac1+mac2+mac3)
 		m += mac1 + mac2 + mac3
 
-	print(m)
+
+
+		cc1 = wi*hi * ceil(ci/options.N) * ceil(cm/options.N)
+		cc2 = wo*ho * 9*ceil(cm/options.N)
+		cc3 = wo*ho * ceil(cm/options.N) * ceil(co/options.N)
+		c+= cc1+cc2+cc3
+
+		print(mac1, mac2, mac3, mac1+mac2+mac3, '    ', cc1, cc2, cc3, cc1+cc2+cc3)
+
+
+	print(m, c)
