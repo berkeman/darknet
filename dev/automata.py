6fbe9ae9fbcb37fa19785a69f0dffad07121e258
Anders Berkeman <anders.berkeman@ericsson.com>
2019-09-19 16:02:25 +0200

    to drougge

--- dev/automata.py
+++ dev/automata.py
@@ -54,15 +54,60 @@ def main(urd):
 
 	jid = urd.build('triplette', jobids=dict(darknet=jid_darknet), datasets=dict(config=jid_type))
 
-	jid = urd.build('triplette_new', jobids=dict(darknet=jid_darknet), datasets=dict(config=jid_type))
-	res = blob.load(jobid=jid)
+	acost = []
 
-	print()
-	print("                        ------   --------------------    --------------------    --------------------")
-	print(" n  maxerr    SNR         X RD     0 RD   0HIT   0MIS     01 RD  01HIT  01MIS     12 RD  12HIT  12MIS")
-	for item in res:
-		print("%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d" % (item[0], item[1], item[2], item[6], item[3][0], item[3][1], item[3][2], item[4][0], item[4][1], item[4][2], item[5][0], item[5][1], item[5][2], ))
+	for c0size, c1size, c2size in (
+#		(    1,1,1),
+#		(112*1,1,1),
+#		(112*2,1,1),
+#		(112*3,1,1),
+
+#		(112*1,100,1),
+#		(112*2,100,1),
+#		(112*3,100,1),
+
+#		(112*1,200,1),
+#		(112*2,200,1),
+#		(112*3,200,1),
+
+#		(112*1,1000,1),
+#		(112*2,1000,1),
+#		(112*3,1000,1),
+
+		(112, 100, 1),
+		(112, 200, 1),
+		(112, 300, 1),
+		(112, 400, 1),
+		(112, 500, 1),
+		(112, 600, 1),
+
+		(1000000,1000000,1),
+	):
+
+		jid = urd.build('triplette_new',
+			jobids=dict(darknet=jid_darknet),
+			datasets=dict(config=jid_type),
+			options=dict(cache0size=c0size, cache01size=c1size, cache12size=c2size)
+		)
+		res = blob.load(jobid=jid)
+
+		xmemsize = 112*112*32
+
+		print()
+		print("                        ------   --------------------    --------------------    --------------------")
+		print(" n  maxerr    SNR         X RD     0 RD   0HIT   0MIS     01 RD  01HIT  01MIS     12 RD  12HIT  12MIS")
+		for item in res:
+			cost = item[6]*xmemsize + item[3][0]*c0size + item[4][0]*c1size + item[5][0]*c2size
+			print(c1size,"%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d  %20s" % (item[0], item[1], item[2], item[6], item[3][0], item[3][1], item[3][2], item[4][0], item[4][1], item[4][2], item[5][0], item[5][1], item[5][2], "{:,}".format(cost)))
+		print("\n")
+
+		acost.append(((c0size, c1size, c2size), item[6]*xmemsize + item[3][0]*c0size + item[4][0]*c1size + item[5][0]*c2size))
+
+	for size, cost in acost:
+		print("%30s %20s" % (size, "{:,}".format(cost)))
 	print("\n")
+
+
 
 # @@@ varför funkar inte sista softmaxlagret?  Verkar inte som att all data sparas i convlayer*.c
 # @@@ saknar 3x3 with stride
