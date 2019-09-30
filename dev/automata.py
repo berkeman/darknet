6622a6d8485f77f72b04a4508b2e045e99dab45b
Anders Berkeman <anders.berkeman@ericsson.com>
2019-09-30 13:09:59 +0200

    [automata] cleanup

--- dev/automata.py
+++ dev/automata.py
@@ -1,6 +1,10 @@
 from extras import resolve_jobid_filename
 import blob
 
+
+m0size = 112*112*32
+
+
 def main(urd):
 
 	jid_darknet = urd.build('darknet')
@@ -35,81 +39,42 @@ def main(urd):
 		)
 	)
 
-#	jid = urd.build('complexity',           datasets=dict(source=jid_type))
-#	jid = urd.build('reslayer_addmemsizes', datasets=dict(reslayers=jid + '/reslayers'))
-#	jid = urd.build('reslayer_ccest',       datasets=dict(source=jid))
-#	jid = urd.build('reslayer_printall',    datasets=dict(source=jid))
-
-
-#	jid = urd.build('emlayer', jobids=dict(darknet=jid_darknet), options=dict(layers=64), datasets=dict(config=jid_type))
-
-#	ev, reads, bdp = blob.load(jobid=jid)
-#	print('sum error', sum(abs(x) for x in ev))
-#	print('max error', max(abs(x) for x in ev))
-#	print('num reads', reads)
-#	print('bdp', bdp)
-
-
-
-
-#	jid = urd.build('triplette', jobids=dict(darknet=jid_darknet), datasets=dict(config=jid_type))
-
-
 	jid_bottlenecks = urd.build('findtriplettes', datasets=dict(config=jid_type))
 
 
 	acost = []
 
 	for c0size, c1size, c2size in (
-
-		(112*1,100,30),
-		(112*2,100,30),
-		(112*3,100,30),
-		(112*4,100,30),
-		(112*5,100,30),
-
-		(112*1,150,30),
-		(112*2,150,30),
-		(112*3,150,30),
-		(112*4,150,30),
-		(112*5,150,30),
-
-		(112*1,125,30),
-		(112*2,125,30),
-		(112*3,125,30),
-		(112*4,125,30),
-		(112*5,125,30),
-
-		(112*1,200,30),
-		(112*2,200,30),
-		(112*3,200,30),
-		(112*4,200,30),
-		(112*5,200,30),
-
-		(112*1,400,30),
-		(112*2,400,30),
-		(112*3,400,30),
-		(112*4,400,30),
-		(112*5,400,30),
-		(1000000,1000000,30),
+		# (112*1,100,30), (112*2,100,30), (112*2.5,100,30), (112*3,100,30), (112*4,100,30), (112*5,100,30),
+		# (112*1,150,30), (112*2,150,30), (112*2.5,150,30), (112*3,150,30), (112*4,150,30), (112*5,150,30),
+		# (112*1,125,30), (112*2,125,30), (112*2.5,125,30), (112*3,125,30), (112*4,125,30), (112*5,125,30),
+		# (112*1,200,30), (112*2,200,30), (112*2.5,200,30), (112*3,200,30), (112*4,200,30), (112*5,200,30),
+		# (112*1,400,30), (112*2,400,30), (112*2.5,400,30), (112*3,400,30), (112*4,400,30), (112*5,400,30),
+		(1000000,1000000,1000000),
 	):
+
+		def cacti(size):
+			jid = urd.build('cacti', options=dict(args=dict(C=size, OUTPUT_WIDTH=32*8, B=32)))
+			return blob.load(jobid=jid)['Dynamic read energy (nJ)']
+
+		costm = cacti(m0size)
+		cost0 = cacti(c0size)
+		cost1 = cacti(c1size)
+		cost2 = cacti(c2size)
 
 		jid = urd.build('triplette_new',
 			jobids=dict(darknet=jid_darknet, bottlenecks=jid_bottlenecks),
-			options=dict(cache0size=c0size, cache01size=c1size, cache12size=c2size)
+			options=dict(xmemsize=m0size, cache0size=c0size, cache01size=c1size, cache12size=c2size)
 		)
 		res = blob.load(jobid=jid)
-
-		xmemsize = 112*112*32
-
 		print()
 		print('=' * 80)
 		print('cache sizes', c0size, c1size, c2size)
-		print("                        ______   ____________________    ____________________    ____________________")
-		print(" n  maxerr    SNR         X RD     0 RD   0HIT   0MIS     01 RD  01HIT  01MIS     12 RD  12HIT  12MIS")
+		print("                        ______   ____________________    ____________________    ____________________    _______________________")
+		print(" n  maxerr    SNR         X RD     0 RD   0HIT   0MIS     01 RD  01HIT  01MIS     12 RD  12HIT  12MIS     cc 1x1  cc 3x3  cc 1x1")
 		for t in sorted(res, key = lambda x: x['n']):
-			t.cost = t.xrcnt*xmemsize + t.c0.reads*c0size + t.c1.reads*c1size + t.c2.reads*c2size
-			print("%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d  %9d %9d %9d %20s" % (
+			t.cost = t.xrcnt*costm + t.c0.reads*cost0 + t.c1.reads*cost1 + t.c2.reads*cost2
+			print("%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d    %7d %7d %7d %20s" % (
 				t.n, t.maxerr, t.snr,
 				t.xrcnt,
 				t.c0.reads, t.c0.hits, t.c0.miss,
@@ -119,7 +84,6 @@ def main(urd):
 				"{:,}".format(t.cost),
 			))
 		print("\n")
-
 		acost.append(res)
 
 
