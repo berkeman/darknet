c4a68933fd9a289aef596e0845a8af8559ee8087
Anders Berkeman <anders.berkeman@ericsson.com>
2019-09-30 14:57:36 +0200

    [automata] prettier print + WL is option

--- dev/automata.py
+++ dev/automata.py
@@ -1,7 +1,9 @@
+from collections import Counter
+
 from extras import resolve_jobid_filename
 import blob
 
-
+WL = 32
 m0size = 112*112*32
 
 
@@ -45,11 +47,12 @@ def main(urd):
 	acost = []
 
 	for c0size, c1size, c2size in (
-		# (112*1,100,30), (112*2,100,30), (112*2.5,100,30), (112*3,100,30), (112*4,100,30), (112*5,100,30),
-		# (112*1,150,30), (112*2,150,30), (112*2.5,150,30), (112*3,150,30), (112*4,150,30), (112*5,150,30),
-		# (112*1,125,30), (112*2,125,30), (112*2.5,125,30), (112*3,125,30), (112*4,125,30), (112*5,125,30),
-		# (112*1,200,30), (112*2,200,30), (112*2.5,200,30), (112*3,200,30), (112*4,200,30), (112*5,200,30),
-		# (112*1,400,30), (112*2,400,30), (112*2.5,400,30), (112*3,400,30), (112*4,400,30), (112*5,400,30),
+#		(112*1,100,30), (112*2,100,30), #(112*2.5,100,30), (112*3,100,30), (112*4,100,30), (112*5,100,30),
+#		(112*1,150,30), (112*2,150,30), #(112*2.5,150,30), (112*3,150,30), (112*4,150,30), (112*5,150,30),
+#		(112*1,125,30), (112*2,125,30), (112*2.5,125,30), (112*3,125,30), (112*4,125,30), (112*5,125,30),
+#		(112*1,200,30), (112*2,200,30), (112*2.5,200,30), (112*3,200,30), (112*4,200,30), (112*5,200,30),
+#		(112*1,400,30), (112*2,400,30), (112*2.5,400,30), (112*3,400,30), (112*4,400,30), (112*5,400,30),
+		(112*3,400,30),
 		(1000000,1000000,1000000),
 	):
 
@@ -57,38 +60,54 @@ def main(urd):
 			jid = urd.build('cacti', options=dict(args=dict(C=size, OUTPUT_WIDTH=32*8, B=32)))
 			return blob.load(jobid=jid)['Dynamic read energy (nJ)']
 
-		costm = cacti(m0size)
-		cost0 = cacti(c0size)
-		cost1 = cacti(c1size)
-		cost2 = cacti(c2size)
+#		costm = cacti(m0size)
+#		cost0 = cacti(c0size)
+#		cost1 = cacti(c1size)
+#		cost2 = cacti(c2size)
+
+		costm = m0size
+		cost0 = c0size
+		cost1 = c1size
+		cost2 = c2size
+
+
 
 		jid = urd.build('bottleneck',
 			jobids=dict(darknet=jid_darknet, bottlenecks=jid_bottlenecks),
-			options=dict(xmemsize=m0size, cache0size=c0size, cache01size=c1size, cache12size=c2size)
+			options=dict(xmemsize=m0size, cache0size=c0size, cache01size=c1size, cache12size=c2size, WL=WL)
 		)
 		res = blob.load(jobid=jid)
+		tot = Counter()
 		print()
 		print('=' * 80)
 		print('cache sizes', c0size, c1size, c2size)
-		print("                        ______   ____________________    ____________________    ____________________    _______________________")
-		print(" n  maxerr    SNR         X RD     0 RD   0HIT   0MIS     01 RD  01HIT  01MIS     12 RD  12HIT  12MIS     cc 1x1  cc 3x3  cc 1x1")
+		print("                        ______   ____________________    ____________________    ____________________    _______________________       ________________     ________________")
+		print(" n  maxerr    SNR         X RD     0 RD   0HIT   0MIS     01 RD  01HIT  01MIS     12 RD  12HIT  12MIS     cc 1x1  cc 3x3  cc 1x1             tot energy     tot clock cycles")
 		for t in sorted(res, key = lambda x: x['n']):
-			t.cost = t.xrcnt*costm + t.c0.reads*cost0 + t.c1.reads*cost1 + t.c2.reads*cost2
-			print("%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d    %7d %7d %7d %20s" % (
+			t.energy = t.xrcnt*costm + t.c0.reads*cost0 + t.c1.reads*cost1 + t.c2.reads*cost2
+			t.cc = t.cc0 + t.cc1 + t.cc2
+			print("%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d    %7d %7d %7d   %20s %20s" % (
 				t.n, t.maxerr, t.snr,
 				t.xrcnt,
 				t.c0.reads, t.c0.hits, t.c0.miss,
 				t.c1.reads, t.c1.hits, t.c1.miss,
 				t.c2.reads, t.c2.hits, t.c2.miss,
 				t.cc0, t.cc1, t.cc2,
-				"{:,}".format(t.cost),
+				"{:,}".format(t.energy),
+				"{:,}".format(t.cc),
 			))
+			tot['cc'] += t.cc
+			tot['e']  += t.energy
+		print("                                                                                                                                   %20s %20s" % (
+			"{:,}".format(tot['e']),
+			"{:,}".format(tot['cc']),
+		))
 		print("\n")
 		acost.append(res)
 
 
 	for item in acost:
-		print(item[0].c0size, item[0].c1size, item[0].c2size, item[0].xsize, sum(x.cost for x in item))
+		print("%9d %9d %9d %9d    %12d %12d" % (item[0].xsize, item[0].c0size, item[0].c1size, item[0].c2size, sum(x.energy for x in item), sum(x.cc for x in item)))
 
 
 
