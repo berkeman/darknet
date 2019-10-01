74b766ea82f6d73a650744dc4b5641b8089cd2a9
Anders Berkeman <anders.berkeman@ericsson.com>
2019-10-01 09:37:01 +0200

    [teststride] appended l2, working

--- dev/a_teststride.py
+++ dev/a_teststride.py
@@ -45,6 +45,7 @@ def synthesis():
 
 		l0 = darknetlayer.Layer(resolve_jobid_filename(jobids.darknet, 'data_layer_%d.txt' % (loepnummer-1,)))
 		l = darknetlayer.Layer(resolve_jobid_filename(jobids.darknet, 'data_layer_%d.txt' % (loepnummer,)))
+		l2 = darknetlayer.Layer(resolve_jobid_filename(jobids.darknet, 'data_layer_%d.txt' % (loepnummer+1,)))
 
 		xmem = memory.Memory(112*112*5, WL)
 		ymem = memory.Memory(112*112*5, WL)
@@ -53,6 +54,8 @@ def synthesis():
 		bias0 = convlayer.BiasNorm(l0)
 		wmem1 = memory.create_weight_mem_3x3dw(l.weights, nwords=WL, channels=l.ci)
 		bias1 = convlayer.BiasNorm(l)
+		wmem2 = memory.create_weight_mem_1x1(l2.weights, nwords=WL, channels_in=l2.ci, channels_out=l2.co)
+		bias2 = convlayer.BiasNorm(l2)
 
 		xmem.importvec(l0.inputs, width=l0.wi, height=l0.hi, channels=l0.ci)
 		xmem.readcnt = 0
@@ -61,8 +64,8 @@ def synthesis():
 		def xmemread(w, h):
 			# return data for all channels at (w, h)
 			v = []
-			for c in range(ceil(l.ci/WL)):
-				a = w + (h * l.wi) + (c * l.wi * l.hi)
+			for c in range(ceil(l0.ci/WL)):
+				a = w + (h * l0.wi) + (c * l0.wi * l0.hi)
 				v.append(xmem.read(a))
 			return v
 
@@ -70,14 +73,16 @@ def synthesis():
 		layer0 = convlayer.Conv1x1_block(cache0.read, wmem0, l0.wi, l0.hi, l0.ci, l0.co, bias0, WL, name='l0')
 		cache01 = cache.FuncCache(1000000, func=layer0.conv)
 		layer = convlayer.Conv3x3dw_block(cache01.read, wmem1, l.wi, l.hi, l.ci, l.stride, bias1, WL, name='l1')
+		cache12 = cache.FuncCache(1000000, func=layer.conv)
+		layer2 = convlayer.Conv1x1_block(cache12.read, wmem2, l2.wi, l2.hi, l2.ci, l2.co, bias2, WL, name='l2')
 
 		# output
-		for h in range(l.ho):
-			for w in range(l.wo):
-				data = layer.conv(w, h)
+		for h in range(l2.ho):
+			for w in range(l2.wo):
+				data = layer2.conv(w, h)
 				for ix, block in enumerate(data):
-					ymem.write(w + h*l.wo + ix * l.wo * l.ho, block)
-		out = ymem.export(width=l.wo, height=l.ho, channels=l.co)
+					ymem.write(w + h*l2.wo + ix * l2.wo * l2.ho, block)
+		out = ymem.export(width=l2.wo, height=l2.ho, channels=l2.co)
 
-		_, _, maxerr, snr = check(out, l.outputs3)
+		_, _, maxerr, snr = check(out, l2.outputs3)
 
