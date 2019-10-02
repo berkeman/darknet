adcc3151b5ebf8b014b5af3bcd522a5cefdd793b
Anders Berkeman <anders.berkeman@ericsson.com>
2019-10-02 17:47:40 +0200

    [automata] better print, cleanup

--- dev/automata.py
+++ dev/automata.py
@@ -3,8 +3,14 @@ from collections import Counter
 from extras import resolve_jobid_filename
 import blob
 
+bitsperword = 8
 WL = 32
-m0size = 112*112*3
+if WL>=24:
+	m0size = 224*224
+else:
+	print("fix m0size, min requirement is 224*224*24 words")
+	exit()
+
 system_freq = 1e9
 
 def main(urd):
@@ -43,18 +49,41 @@ def main(urd):
 
 	jid_bottlenecks = urd.build('findtriplettes', datasets=dict(config=jid_type))
 
-
 #	jid = urd.build('teststride', jobids=dict(darknet=jid_darknet))
 
-	acost = []
+	urd.build('addmemsizes', datasets=dict(config=jid_type))
+
+	jid_macs = urd.build('countmacs',
+		jobids=dict(bottlenecks=jid_bottlenecks),
+		datasets=dict(config=jid_type),
+	)
+	x = blob.load(jobid=jid_macs)
+	botmacs = sum(x[0].values())
+	skipmacs = sum(x[1].values())
+	print('MACs in bottleneck layers', botmacs)
+	print('MACs in skipped layers   ', skipmacs)
+	print('skipped ratio            ', skipmacs / (skipmacs + botmacs))
+
+
+
+	costs = []
 
 	for c0size, c1size, c2size in (
 
-#		(112*4,400,30),# (112*3,400,30), (112*2.5,400,30), (112*2,400,30), (112*1,400,30),
-#		(112*4,300,30),# (112*3,300,30), (112*2.5,300,30), (112*2,300,30), (112*1,300,30),
-#		(112*4,200,30), (112*3,200,30), (112*2.5,200,30), (112*2,200,30), (112*1,200,30),
+		(112*1,10000,30),
+		(112*2,10000,30),
+		(112*3,10000,30),
+		(112*4,10000,30),
 
-#		(m0size, m0size, m0size),
+		(112*4,1000,30),
+		(112*4,500,30),
+		(112*4,400,30),
+		(112*4,300,30),
+		(112*4,200,30),
+
+		(112*1,400,30),
+
+		(m0size, m0size, m0size),
 		(1e6, 1e6, 1e6),
 
 #		(1e6, m0size, m0size),
@@ -78,17 +107,6 @@ def main(urd):
 
 		MUL1 = 1
 		MUL3 = 1
-
-		jid_macs = urd.build('countmacs',
-			jobids=dict(bottlenecks=jid_bottlenecks),
-			datasets=dict(config=jid_type),
-		)
-		x = blob.load(jobid=jid_macs)
-		botmacs = sum(x[0].values())
-		skipmacs = sum(x[1].values())
-		print('MACs in bottleneck layers', botmacs)
-		print('MACs in skipped layers   ', skipmacs)
-		print('skipped ratio            ', skipmacs / (skipmacs + botmacs))
 
 
 		def colmag(f):
@@ -119,9 +137,8 @@ def main(urd):
 			t.l0stat['cc'] /= MUL1
 			t.l1stat['cc'] /= MUL3
 			t.l2stat['cc'] /= MUL1
-
 			t.cc = t.l0stat['cc'] + t.l1stat['cc'] + t.l2stat['cc']
-			print("%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d    %7d %7d %7d   %20s %20s" % (
+			print("%2d  %f  %5.2f    %6d   %6d %6d %6d    %6d %6d %6d    %6d %6d %6d    %7d %7d %7d   %20s %20s   %s" % (
 				t.n, t.maxerr, t.snr,
 				t.xrcnt,
 				t.c0.reads, t.c0.hits, t.c0.miss,
@@ -130,9 +147,23 @@ def main(urd):
 				t.l0stat['cc'], t.l1stat['cc'], t.l2stat['cc'],
 				"{:,}".format(t.energy),
 				"{:,}".format(t.cc),
+				"%dx%d %d->%d->%d %dx%d" % (t.l0stat['wi'], t.l0stat['hi'], t.l0stat['ci'], t.l2stat['ci'], t.l2stat['co'], t.l2stat['wo'], t.l2stat['ho'],),
 			))
-
-			print("                        %6s                 %6s                  %6s                  %6s     %6s  %6s  %6s" % (
+			tot['cc'] += t.cc
+			tot['e']  += t.energy
+		print("                                                                                                                                   %20s %20s" % (
+			"{:,}".format(tot['e']),
+			"{:,}".format(tot['cc']),
+		))
+		print()
+		print("                                                                                                                                                            layer      featin   featout")
+		for t in sorted(res, key = lambda x: x['n']):
+			t.energy = t.xrcnt*costm + t.c0.reads*cost0 + t.c1.reads*cost1 + t.c2.reads*cost2
+			t.l0stat['cc'] /= MUL1
+			t.l1stat['cc'] /= MUL3
+			t.l2stat['cc'] /= MUL1
+			t.cc = t.l0stat['cc'] + t.l1stat['cc'] + t.l2stat['cc']
+			print("                        %6s                 %6s                  %6s                  %6s     %6s  %6s  %6s   %30s   %9d %9d" % (
 				colmag(t.xrcnt   / (t.l0stat['wi']*t.l0stat['hi']*t.l0stat['ci']/t.l0stat['WL'])),
 				colmag(t.c0.miss / (t.l0stat['wi']*t.l0stat['hi'])),
 				colmag(t.c1.miss / (t.l1stat['wi']*t.l1stat['hi'])),
@@ -140,28 +171,27 @@ def main(urd):
 				colmag(MUL1 * WL * t.l0stat['cc'] / (t.l0stat['wi']*t.l0stat['hi']*t.l0stat['ci']*t.l0stat['co'])),
 				colmag(MUL3 * WL * t.l1stat['cc'] / (t.l1stat['wo']*t.l1stat['ho']*t.l1stat['co']* 3 * 3)),
 				colmag(MUL1 * WL * t.l2stat['cc'] / (t.l2stat['wi']*t.l2stat['hi']*t.l2stat['ci']*t.l2stat['co'])),
+				"%dx%d %d->%d->%d %dx%d" % (t.l0stat['wi'], t.l0stat['hi'], t.l0stat['ci'], t.l2stat['ci'], t.l2stat['co'], t.l2stat['wo'], t.l2stat['ho'],),
+				t.l0stat['wi']*t.l0stat['hi']*t.l0stat['ci'],
+				t.l1stat['wi']*t.l1stat['hi']*t.l1stat['ci'],
 			))
-
-			tot['cc'] += t.cc
-			tot['e']  += t.energy
-		print("                                                                                                                                   %20s %20s" % (
-			"{:,}".format(tot['e']),
-			"{:,}".format(tot['cc']),
-		))
 		print("\n")
-		acost.append(res)
 
-	for item in acost:
-		print("%9d %9d %9d %9d    %12d %12d  %5.2ffps" % (item[0].xsize, item[0].c0size, item[0].c1size, item[0].c2size, sum(x.energy for x in item), sum(x.cc for x in item), system_freq/sum(x.cc for x in item)))
+
+		costs.append(res)
+
+	print("     xmem        c0         c1         c2           energy           cc      rate")
+	for item in costs:
+		print("%9d %10d %10d %10d    %12d %12d  %5.2ffps" % (
+			item[0].xsize*WL*bitsperword,
+			item[0].c0size*WL*bitsperword,
+			item[0].c1size*WL*bitsperword,
+			item[0].c2size*WL*bitsperword,
+			sum(x.energy for x in item),
+			sum(x.cc for x in item),
+			system_freq/sum(x.cc for x in item)
+		))
+	print("All memory sizes in *bits*")
 
 	from automata_common import profile_jobs
-	print('Exec time', profile_jobs(urd.joblist))
-
-
-
-# @@@ varför funkar inte sista softmaxlagret?  Verkar inte som att all data sparas i convlayer*.c
-# @@@ saknar 3x3 with stride
-
-# @@@ kolla hur och varför det kraschar när cachen är "liten"
-
-# @@@ räkna klockcykler tillsammans med block-aritmetiken på ngt vis, tänk på cache12 som kanske är nio disjunkta minnen!
+	print('\nExec time', profile_jobs(urd.joblist))
